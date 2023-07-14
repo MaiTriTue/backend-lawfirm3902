5082512d1bb1c9dcbb51f26e01b5a91da0fc7787
Mai Trí Tuệ <93773841+MaiTriTue@users.noreply.github.com>
2023-07-14 22:08:19 +0700

    svagger

--- origin/urls.py
+++ origin/urls.py
@@ -1,23 +1,24 @@
-"""
-URL configuration for lawfirm project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from . import views
+from .admin import admin_site
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('user', views.UserViewSet, basename='user')
+router.register('category', views.CategoryViewSet, basename='category')
+router.register('SubCategory', views.SubCategoryViewSet, basename='SubCategory')
+router.register('Posts', views.PostViewSet, basename='Posts')
+router.register('contact', views.ContactViewSet, basename='contact')
+router.register('introduce', views.IntroduceViewSet, basename='introduce')
+router.register('tagfield', views.TagFieldViewSet, basename='tagfield')
+
+
+
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('admin/', admin_site.urls),
+    # path('', views.index, name="index"),
+    path('', include(router.urls)),
+
 ]
+
